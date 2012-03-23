c0c4715551a5d0cdcb5ac54b5d60d40923e18feb
logaritmisk <anders.e.olsson@gmail.com>
2012-03-23 14:33:23 -0600

    hate tabs

--- TableCommit.user.js
+++ TableCommit.user.js
@@ -8,18 +8,18 @@
 
 
 function $(callback) {
+    var script = document.createElement("script");
+
+    script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+    script.addEventListener('load', function() {
 	var script = document.createElement("script");
 
-	script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
-	script.addEventListener('load', function() {
-		var script = document.createElement("script");
-
-		script.textContent = "(" + callback.toString() + ")();";
-
-		document.body.appendChild(script);
-	}, false);
+	script.textContent = "(" + callback.toString() + ")();";
 
 	document.body.appendChild(script);
+    }, false);
+
+    document.body.appendChild(script);
 }
 
 $(function() {
