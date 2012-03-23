22fde9a2f916f843a90da96f0c848b0044d0efb6
logaritmisk <anders.e.olsson@gmail.com>
2012-03-23 13:21:53 -0600

    update script to get it working with chrome

--- TableCommit.user.js
+++ TableCommit.user.js
@@ -1,23 +1,41 @@
 // ==UserScript==
 // @name           TableCommit
 // @namespace      http://drupalcode.org/
-// @version        1.0.0
+// @version        1.1.0
 // @updateURL      https://github.com/Logaritmisk/Userscripts/raw/master/TableCommit.user.js
 // @include        http://drupalcode.org/*
-// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 // ==/UserScript==
 
 
-var DEFAULT_CSS = {
-    'font-size': '9px',
-    'margin-left': '10px',
-    'color': '#aaa',
+function $(callback) {
+	var script = document.createElement("script");
+
+	script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+	script.addEventListener('load', function() {
+		var script = document.createElement("script");
+
+		script.textContent = "(" + callback.toString() + ")();";
+
+		document.body.appendChild(script);
+	}, false);
+
+	document.body.appendChild(script);
 }
 
-$('table.shortlog a.subject').each(function(i, e) {
-    var commit = $('<span>')
-	.css(DEFAULT_CSS)
-	.text($(e).attr('href').substr(-40));
+$(function() {
 
-    $(e).parent().append(commit);
+    var DEFAULT_CSS = {
+        'font-size': '9px',
+        'margin-left': '10px',
+        'color': '#aaa',
+    }
+
+    $('table.shortlog a.subject').each(function(i, e) {
+        var commit = $('<span>')
+	    .css(DEFAULT_CSS)
+	    .text($(e).attr('href').substr(-40));
+
+        $(e).parent().append(commit);
+    });
+
 });
