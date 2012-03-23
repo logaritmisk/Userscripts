c500996e3ca1479af162b75743179c399739a96a
logaritmisk <anders.e.olsson@gmail.com>
2012-03-22 19:55:35 -0600

    first commit

---
+++ Module_machine_name.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           Module machine_name
+// @namespace      Drupal-7.x
+// @include        http://*/admin/modules
+// ==/UserScript==
+
+
+var $ = unsafeWindow.jQuery;
+
+
+$('#system-modules :checkbox').each(function(i, e) {
+	var name = $('<div>')
+		.addClass('admin-requirements')
+		.html(':' + $(e).attr('name').split('][', 3)[1]);
+
+	$('label', $(e).parents('tr'))
+		.parent()
+		.append(name);
+});
