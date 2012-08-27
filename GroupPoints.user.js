b302da9eefc0391973910847ed7ed7543633532e
logaritmisk <anders.e.olsson@gmail.com>
2012-08-27 13:56:15 +0200

    update to work with chrome

--- GroupPoints.user.js
+++ GroupPoints.user.js
@@ -2,7 +2,7 @@
 // @name        GroupPoints
 // @namespace   activecollab.com
 // @include     http://ac3.nodeone.se/*
-// @version     1.0.0
+// @version     1.1.0
 // @grant       none
 // @updateURL   https://github.com/Logaritmisk/Userscripts/raw/master/GroupPoints.user.js
 // ==/UserScript==
@@ -11,28 +11,53 @@
 var $ = unsafeWindow.jQuery;
 
 
-$('div#tasks div.objects_list')
-    .change(update_points)
-    .change();
+function require(callback) {
+    if (typeof $ != undefined) {
+        callback();
 
+        return;
+    }
 
-function update_points() {
-    $('div#tasks div.object_list_group > div.object_list_group_head').each(function(i, e) {
-	var $count = $('span.object_list_group_count', e);
+    var script = document.createElement("script");
 
-	if ($count.length) {
-	    var $points = 0;
-	    var $pattern = /\[([0-9].*)p\]/i;
+    script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+    script.addEventListener('load', function() {
+	var script = document.createElement("script");
 
-	    $('+ div.object_list_group_body .real_task_name', e).each(function(n, t) {
-		var $point = $(t).text().match($pattern);
+	script.textContent = "(" + callback.toString() + ")();";
 
-		if ($point) {
-					$points += parseInt($point[1]);
-		}
-	    });
+	document.body.appendChild(script);
+    }, false);
 
-	    $count.text($count.text() + ', ' + $points + 'p');
-	}
-    });
+    document.body.appendChild(script);
 }
+
+require(function() {
+
+    $('div#tasks div.objects_list')
+	.change(update_points)
+	.change();
+
+
+    function update_points() {
+	$('div#tasks div.object_list_group > div.object_list_group_head').each(function(i, e) {
+	    var $count = $('span.object_list_group_count', e);
+
+	    if ($count.length) {
+		var $points = 0;
+		var $pattern = /\[([0-9].*)p\]/i;
+
+		$('+ div.object_list_group_body .real_task_name', e).each(function(n, t) {
+		    var $point = $(t).text().match($pattern);
+
+		    if ($point) {
+			$points += parseInt($point[1]);
+		    }
+		});
+
+		$count.text($count.text() + ', ' + $points + 'p');
+	    }
+	});
+    }
+
+});
