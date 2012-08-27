13ef52f08e541927a468fb54585b0c77c3a34503
logaritmisk <anders.e.olsson@gmail.com>
2012-08-27 13:50:53 +0200

    smaaall changes, added update url

--- GroupPoints.user.js
+++ GroupPoints.user.js
@@ -4,34 +4,35 @@
 // @include     http://ac3.nodeone.se/*
 // @version     1.0.0
 // @grant       none
+// @updateURL   https://github.com/Logaritmisk/Userscripts/raw/master/GroupPoints.user.js
 // ==/UserScript==
 
 
-var $ = unsafeWindow.$;
+var $ = unsafeWindow.jQuery;
 
 
 $('div#tasks div.objects_list')
-	.change(update_points)
-	.change();
+    .change(update_points)
+    .change();
 
 
 function update_points() {
-	$('div#tasks div.object_list_group > div.object_list_group_head').each(function(i, e) {
-		var $count = $('span.object_list_group_count', e);
+    $('div#tasks div.object_list_group > div.object_list_group_head').each(function(i, e) {
+	var $count = $('span.object_list_group_count', e);
 
-		if ($count.length) {
-			var $points = 0;
-			var $pattern = /\[([0-9].*)p\]/i;
-		
-			$('+ div.object_list_group_body .real_task_name', e).each(function(n, t) {
-				var $point = $(t).text().match($pattern);
+	if ($count.length) {
+	    var $points = 0;
+	    var $pattern = /\[([0-9].*)p\]/i;
 
-				if ($point) {
+	    $('+ div.object_list_group_body .real_task_name', e).each(function(n, t) {
+		var $point = $(t).text().match($pattern);
+
+		if ($point) {
 					$points += parseInt($point[1]);
-				}
-			});
-
-			$count.text($count.text() + ', ' + $points + 'p');
 		}
-	})
+	    });
+
+	    $count.text($count.text() + ', ' + $points + 'p');
+	}
+    });
 }
