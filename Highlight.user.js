8c9f0201b59c043d0e4e35eea81f8a65c151e63a
logaritmisk <anders.e.olsson@gmail.com>
2013-08-26 14:43:56 +0200

    sweet

---
+++ Highlight.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Highlight
+// @namespace   logaritmisk
+// @include     *.module
+// @version     0.0.1
+// @grant       none
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @require     http://yandex.st/highlightjs/7.3/highlight.min.js
+// ==/UserScript==
+
+
+(function($) {
+
+  $('head').append('<link rel="stylesheet" href="http://yandex.st/highlightjs/7.3/styles/github.min.css">');
+	
+	$('pre').each(function(i, e) {
+		hljs.highlightBlock(e);
+	});
+
+})(jQuery);
