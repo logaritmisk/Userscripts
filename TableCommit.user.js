8f9a54fe8bce5ff52296f7e53addde68a616d545
logaritmisk <anders.e.olsson@gmail.com>
2014-02-14 11:25:56 +0100

    use require instead of load

--- TableCommit.user.js
+++ TableCommit.user.js
@@ -1,36 +1,44 @@
 // ==UserScript==
 // @name           TableCommit
 // @namespace      http://drupalcode.org/
-// @version        1.1.1
+// @version        1.1.2
 // @updateURL      https://github.com/Logaritmisk/Userscripts/raw/master/TableCommit.user.js
-// @include        http://drupalcode.org/*
+// @include        http://drupalcode.org/*.git
+// @include        http://drupalcode.org/*.git/shortlog/*
 // ==/UserScript==
 
-function loadZepto(callback) {
-  var script = document.createElement("script");
 
-  script.setAttribute("src", "http://zeptojs.com/zepto.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    script.textContent = "(" + callback.toString() + ")();";
-    document.body.appendChild(script);
-  }, false);
+function require(sources, callback) {
+    counter = sources.length
+    sources.forEach(function(source) {
+        var script = document.createElement('script');
 
-  document.body.appendChild(script);
+        script.setAttribute('src', source);
+        script.addEventListener('load', function() {
+            if (--counter == 0) {
+                var script = document.createElement('script');
+                script.textContent = '(' + callback.toString() + ')();';
+                document.body.appendChild(script);
+            }
+        }, false);
+
+        document.body.appendChild(script);
+    });
 }
 
-loadZepto(function() {
-  var DEFAULT_CSS = {
-    'font-size': '9px',
-    'margin-left': '10px',
-    'color': '#aaa',
-  }
 
-  $('table.shortlog a.subject').each(function(i, e) {
-    var commit = $('<span>')
-      .css(DEFAULT_CSS)
-      .text($(e).attr('href').substr(-40));
+require(['http://zeptojs.com/zepto.min.js'], function() {
+    var DEFAULT_CSS = {
+        'font-size': '9px',
+        'margin-left': '10px',
+        'color': '#aaa',
+    }
 
-    $(e).parent().append(commit);
-  });
+    $('table.shortlog a.subject').each(function(i, e) {
+        var commit = $('<span>')
+                .css(DEFAULT_CSS)
+                .text($(e).attr('href').substr(-40));
+
+        $(e).parent().append(commit);
+    });
 });
