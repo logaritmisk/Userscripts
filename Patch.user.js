99055e195df518f393f4023028fec6a8f8c2e2ea
Lars Stadel Linnet <larslinnet@gmail.com>
2014-05-20 10:43:58 +0200

    Support patches served on https
    Drupal.org uses https to serve files, so in order for this to work when clicking a patch on d.o it has to use https urls - this should not affect standard http calls.

--- Patch.user.js
+++ Patch.user.js
@@ -4,14 +4,14 @@
 // @include     *.patch
 // @version     0.0.1
 // @grant       none
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
-// @require     http://yandex.st/highlightjs/7.3/highlight.min.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @require     https://yandex.st/highlightjs/7.3/highlight.min.js
 // ==/UserScript==
 
 
 (function($) {
 
-  $('head').append('<link rel="stylesheet" href="http://yandex.st/highlightjs/7.3/styles/github.min.css">');
+  $('head').append('<link rel="stylesheet" href="https://yandex.st/highlightjs/7.3/styles/github.min.css">');
 	
 	$('pre').each(function(i, e) {
 		$(this).addClass('diff');
