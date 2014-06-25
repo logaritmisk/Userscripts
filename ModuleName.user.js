fdc5cee63a028f78b47b957af84b8cdd0bf40414
logaritmisk <anders.e.olsson@gmail.com>
2014-06-25 12:08:51 +0200

    use require function instead

--- ModuleName.user.js
+++ ModuleName.user.js
@@ -1,27 +1,34 @@
 // ==UserScript==
 // @name        ModuleName
 // @namespace   http://drupal.org/
-// @version     1.1.1
+// @version     1.1.2
 // @updateURL   https://github.com/Logaritmisk/Userscripts/raw/master/ModuleName.user.js
 // @include     */?q=admin/modules
 // @include     */admin/modules
 // @grant       none
 // ==/UserScript==
 
-function loadZepto(callback) {
-  var script = document.createElement("script");
 
-  script.setAttribute("src", "http://zeptojs.com/zepto.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    script.textContent = "(" + callback.toString() + ")();";
+function require(sources, callback) {
+  counter = sources.length
+  sources.forEach(function(source) {
+    var script = document.createElement('script');
+
+    script.setAttribute('src', source);
+    script.addEventListener('load', function() {
+      if (--counter == 0) {
+        var script = document.createElement('script');
+        script.textContent = '(' + callback.toString() + ')();';
+        document.body.appendChild(script);
+      }
+    }, false);
+
     document.body.appendChild(script);
-  }, false);
-
-  document.body.appendChild(script);
+  });
 }
 
-loadZepto(function() {
+
+require(['//cdnjs.cloudflare.com/ajax/libs/zepto/1.1.3/zepto.min.js'], function() {
   $('#system-modules input[type="checkbox"]').each(function(i, e) {
     var name = $('<div>')
       .addClass('admin-requirements')
